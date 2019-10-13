5cb623956040e14e8a80cc06c4251483b8883e10
Jigar soni <jigarsoni0008@gmail.com>
2019-10-13 11:53:53 +1100

    add feature to edit blog post from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 
     path('post/new/', views.post_new, name='new_post'),
+
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
 ]
