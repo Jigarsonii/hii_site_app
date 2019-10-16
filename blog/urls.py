96fd51f8fdf58e46a77e665bb0000b2b1c6bf03c
Jigar soni <jigarsoni0008@gmail.com>
2019-10-16 23:45:11 +1100

    add feature to make drafts and publish them

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('post/new/', views.post_new, name='new_post'),
 
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
+
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
